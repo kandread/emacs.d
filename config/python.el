1b6823b8a2db02fe2e95fa92329a37a507513b19
Kostas Andreadis <kandread@umass.edu>
2019-01-03 09:18:28 -0500

    initial configuration of Python

---
+++ config/python.el
@@ -0,0 +1,26 @@
+;; -*- lexical-binding: t; -*-
+
+(defun +python/kill-inferior-on-exit ()
+  "Kill buffer and delete window of inferior process."
+  (let* ((buff (current-buffer))
+          (proc (get-buffer-process buff)))
+    (lexical-let ((buff buff))
+      (set-process-sentinel proc (lambda (process event)
+                                   (if (string= event "finished\n")
+                                     (kill-buffer-and-window)))))))
+
+(use-package anaconda-mode
+  :hook python-mode
+  :init
+  (setq python-indent-offset 4
+    anaconda-mode-eldoc-as-single-line t)
+  :config
+  (add-hook 'python-mode-hook #'anaconda-eldoc-mode)
+  (add-hook 'inferior-python-mode-hook #'+python/kill-inferior-on-exit))
+
+(use-package company-anaconda
+  :after company
+  :config
+  (add-to-list 'company-backends 'company-anaconda))
+
+
