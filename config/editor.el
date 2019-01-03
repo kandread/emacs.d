0d2a3014a2d241287a992bf81be8da49615f6743
Kostas Andreadis <kandread@umass.edu>
2019-01-03 09:12:45 -0500

    jump to things and highlight delimiters

---
+++ config/editor.el
@@ -0,0 +1,17 @@
+;; -*- lexical-binding: t; -*-
+
+;; Highlight matching delimiters
+(setq show-paren-delay 0.1
+  show-paren-highlight-openparen t
+  show-paren-when-point-inside-paren t)
+(show-paren-mode 1)
+
+;; Jump to things in Emacs tree-style
+(use-package avy
+  :commands (avy-goto-char-2 avy-goto-line avy-goto-word-1)
+  :bind (("M-g 2" . avy-goto-char-2)
+          ("M-g l" . avy-goto-line)
+          ("M-g w" . avy-goto-word-1)))
+
+;; Fix Lisp indent offset
+(setq lisp-indent-offset 2)
