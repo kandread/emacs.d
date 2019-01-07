157aac3e4332598d52446e4ab66caecd3c6bcf19
Kostas Andreadis <kandread@umass.edu>
2019-01-07 15:34:07 -0500

    additional GTD configuration

--- config/org.el
+++ config/org.el
@@ -4,9 +4,8 @@
 (use-package org
   :demand t
   :ensure org-plus-contrib
+  :bind ("C-x c" . org-capture)
   :config
-  ;; activate easy template expansion
-  ;; (require 'org-tempo)
   ;; set up interface
   (setq-default
     org-startup-folded t
@@ -31,6 +30,8 @@
     '((python . t)))
   ;; set org file directory
   (setq org-files-directory "~/Documents/Org/")
+  ;; set agenda files
+  (setq org-agenda-files (list org-files-directory))
   ;; set default notes file
   (setq org-default-notes-file (expand-file-name "notes.org" org-files-directory))
   ;; set capture templates
@@ -45,16 +46,40 @@
          "* %? \n%^T\n%a\n")))
   ;; set task states
   (setq org-todo-keywords
-    (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-	     (sequence "WAITING(w@/!)" "|" "SOMEDAY(o)" "CANCELLED(c@/!)"))))
+    '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+       (sequence "WAITING(w@/!)" "|" "SOMEDAY(o)" "CANCELLED(c@/!)")))
   ;; trigger task states
   (setq org-todo-state-tags-triggers
-    (quote (("CANCELLED" ("CANCELLED" . t))
-             ("WAITING" ("WAITING" . t))
-             (done ("WAITING"))
-             ("TODO" ("WAITING") ("CANCELLED"))
-             ("NEXT" ("WAITING") ("CANCELLED"))
-	     ("DONE" ("WAITING") ("CANCELLED"))))))
+    '(("CANCELLED" ("CANCELLED" . t))
+       ("WAITING" ("WAITING" . t))
+       (done ("WAITING"))
+       ("TODO" ("WAITING") ("CANCELLED"))
+       ("NEXT" ("WAITING") ("CANCELLED"))
+       ("DONE" ("WAITING") ("CANCELLED"))))
+  ;; exclude PROJECT tag from being inherited
+  (setq org-tags-exclude-from-inheritance '("project"))
+  ;; show inherited tags in agenda view
+  (setq org-agenda-show-inherited-tags t)
+  ;; set archive tag
+  (setq org-archive-tag "archive")
+  ;; set archive file
+  (setq org-archive-location (concat org-files-directory "archive.org::* From %s"))
+  ;; refiling targets include any file contributing to the agenda - up to 2 levels deep
+  (setq org-refile-targets '((nil :maxlevel . 2)
+                              (org-agenda-files :maxlevel . 2)))
+  ;; show refile targets simultaneously
+  (setq org-outline-path-complete-in-steps nil)
+  ;; use full outline paths for refile targets
+  (setq org-refile-use-outline-path 'file)
+  ;; allow refile to create parent tasks with confirmation
+  (setq org-refile-allow-creating-parent-nodes 'confirm)
+  ;; exclude done tasks from refile targets
+  (setq org-refile-target-verify-function #'+org/verify-refile-target))
 
 ;; Export libraries
 (use-package htmlize)
+
+;; Functions
+(defun +org/verify-refile-target ()
+  "Exclude todo keywords with a done state from refile targets"
+  (not (member (nth 2 (org-heading-components)) org-done-keywords)))
