5087ae0475075941c40bcc1d81ff857a1cebfbf3
Kostas Andreadis <kandread@umass.edu>
2019-01-16 09:19:50 -0500

    remap functions with counsel-projectile

--- config/project.el
+++ config/project.el
@@ -12,5 +12,16 @@
 
 ;; Ivy UI for Projectile
 (use-package counsel-projectile
-  :after projectile)
+  :commands (counsel-projectile-find-file counsel-projectile-find-dir counsel-projectile-switch-to-buffer
+	      counsel-projectile-grep counsel-projectile-ag counsel-projectile-switch-project)
+  :init
+  (progn
+    (define-key projectile-mode-map [remap projectile-find-file] #'counsel-projectile-find-file)
+    (define-key projectile-mode-map [remap projectile-find-dir] #'counsel-projectile-find-dir)
+    (define-key projectile-mode-map [remap projectile-switch-to-buffer] #'counsel-projectile-switch-to-buffer)
+    (define-key projectile-mode-map [remap projectile-grep] #'counsel-projectile-grep)
+    (define-key projectile-mode-map [remap projectile-ag] #'counsel-projectile-ag)
+    (define-key projectile-mode-map [remap projectile-switch-project] #'counsel-projectile-switch-project))
+  :config
+  (ivy-set-display-transformer #'counsel-projectile-find-file nil))
     
