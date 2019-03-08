e19fdbe46fc2751d6cfd4c3450478e46f809cbd2
Kostas Andreadis <kandread@umass.edu>
2019-03-08 06:43:20 -0500

    initial configuration for LaTeX

--- config/writing.el
+++ config/writing.el
@@ -6,3 +6,19 @@
   (unless noninteractive
     (pdf-tools-install))
   (setq-default pdf-view-display-size 'fit-page))
+
+(use-package tex
+  :ensure auctex
+  :config
+  (setq TeX-parse-self t ; parse on load
+    TeX-auto-save t ; parse on save
+    ;; use hidden dirs for auctex files
+    TeX-auto-local ".auctex-auto"
+    TeX-style-local ".auctex-style"
+    TeX-source-correlate-mode t
+    TeX-source-correlate-method 'synctex
+    ;; don't start the emacs server when correlating sources
+    TeX-source-correlate-start-server nil
+    ;; automatically insert braces after sub/superscript in math mode
+    TeX-electric-sub-and-superscript t)
+  (setq-local ispell-parser 'tex))
